ad02c1aca3f9cf7faa9c1d2676752cbefd855ec9
Saurabh Ariyan <saurabhkumar380@gmail.com>
2021-08-07 18:00:15 +0530

    added drf views

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6^lb3+z-+60rqq34g=m$p45ha1lq5v3v(!l0k5on=8w47#wts+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- dataparser/views.py
+++ dataparser/views.py
@@ -1,7 +1,8 @@
 import logging
 
 from django.conf import settings
-from rest_framework import generics, status
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from dataparser.load_data import parse_file
@@ -9,16 +10,20 @@ from dataparser.load_data import parse_file
 log = logging.getLogger(__name__)
 
 
-datafiles = {"BANK_MARKETING": settings.BASE_DIR.joinpath("data/bank-marketing.json"),
+datafiles = {"BANK-MARKETING": settings.BASE_DIR.joinpath("data/bank-marketing.json"),
              "ECOMMERCE": settings.BASE_DIR.joinpath("data/ecommerce.json")}
 
 
 # Create your views here.
-class DimensionListView(generics.ListAPIView):
-    def list(self):
+class DimensionListView(GenericAPIView):
+    def get_queryset(self):
+        return None
+
+    def get(self, request, **kwargs):
         try:
-            file_name = self.kwargs["filename"]
-            if file_name.upper() in ("BANK_MARKETING", "ECOMMERCE"):
+            file_name = kwargs["filename"]
+            print(file_name)
+            if file_name.upper() in datafiles.keys():
                 return Response(data=parse_file(datafiles[file_name.upper()]), status=status.HTTP_200_OK)
             else:
                 return Response(data={"error": "filename must be either bank_marketing or ecommerce"}, status=status.HTTP_400_BAD_REQUEST)
