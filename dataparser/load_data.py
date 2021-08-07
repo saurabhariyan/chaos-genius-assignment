5ad890d69bf2255e99a9ec84a8baf77ab4664381
Saurabh Ariyan <saurabhkumar380@gmail.com>
2021-08-07 17:04:32 +0530

    added logger

--- dataparser/load_data.py
+++ dataparser/load_data.py
@@ -1,10 +1,12 @@
-
 import json
-from django.conf import settings
+import logging
 from pathlib import PosixPath
+
+from django.conf import settings
 
 BANK_MARKETING_FILE_PATH = settings.BASE_DIR.joinpath("data/bank-marketing.json")
 ECOMMERCE_FILE_PATH = settings.BASE_DIR.joinpath("data/ecommerce.json")
+log = logging.getLogger(__name__)
 
 
 def parsing_multi_dimesions(x: list) -> list:
@@ -16,14 +18,19 @@ def flatten_list_uniques(z: list) -> list:
 
 
 def parsing_single_dimesions(y: list) -> list:
+    # isalpha handles all the other equalites <=, >=. etc
     return "".join([c for c in y.split("=")[0]if c.isalpha()])
 
 
 def parse_file(file_name: PosixPath) -> list:
     dimesions = set()
-    f = open(file_name)
-    data = json.load(f)
-    x1 = list(map(parsing_multi_dimesions, data))
-    x2 = flatten_list_uniques(x1)
-    dimesions = set(list(map(parsing_single_dimesions, x2)))
-    return list(dimesions)
+    try:
+        f = open(file_name)
+        data = json.load(f)
+        x1 = list(map(parsing_multi_dimesions, data))
+        x2 = flatten_list_uniques(x1)
+        dimesions = set(list(map(parsing_single_dimesions, x2)))
+    except Exception as e:
+        log.error(e)
+    finally:
+        return list(dimesions)
